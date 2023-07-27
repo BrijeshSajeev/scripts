74749b108c40c5aec57416f506fe9b6a1363a78b
BrijeshSajeev <brijesh00300@gmail.com>
2023-07-27 23:05:43 +0530

    new:subprocess

---
+++ subprocess_func.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+#subprocess.run(["ls"])
+subprocess.run(["python3","--version"])
+
+
+"""
+* used to run new codes and applications by creating new processes
+*  It also helps to obtain the input/output/error pipes as well as the 
+exit codes of various commands.
+
+
+"""
+
+

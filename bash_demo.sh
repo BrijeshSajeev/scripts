c430549fdc9a5feb44a69006a13eff141aefca6f
BrijeshSajeev <brijesh00300@gmail.com>
2023-07-30 20:01:57 +0530

    new:bash

---
+++ bash_demo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Starting time : $(date)"
+
+echo "UPTIME"; uptime; echo "--------------"
+echo "FREE"; free; echo "--------------"
+echo "who"; who; echo "--------------"
+
+echo "Ending time : $(date)"
+
